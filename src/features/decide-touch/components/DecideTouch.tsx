4de9f9fb7a94ee4938e8325d4361ac5210a73d37
tavish <tapegram@gmail.com>
2024-04-14 21:11:55 -0500

    Buffering videos with a hidden video player

--- src/features/decide-touch/components/DecideTouch.tsx
+++ src/features/decide-touch/components/DecideTouch.tsx
@@ -3,49 +3,101 @@ import { ClipDecision, Decision, TouchClip } from "../domain";
 import usePaginatedClipsFetcher from "../hooks/usePaginatedClipsFetcher";
 import useDecision from "../hooks/useMakeDecision";
 
+type CurrClip = "A" | "B";
+/**
+ *
+ * To handle swapping in the next video without thrashing in resizing, and also to minimize lag when loading the video
+ * we have two video tags running, with one hidden and the alternate being preloaded.
+ */
 const DecideTouch = () => {
   const clips = usePaginatedClipsFetcher();
-  const [currentClip, setCurrentClip] = useState<TouchClip>(clips.next().value);
+  const [clipA, setClipA] = useState<TouchClip>(clips.next().value);
+  const [clipB, setClipB] = useState<TouchClip>(clips.next().value);
+  const [currClip, setCurrClip] = useState<CurrClip>("A");
   const [getDecisions, makeDecision] = useDecision();
 
+  const getCurrentClip = () => (currClip == "A" ? clipA : clipB);
+
+  const toggleCurrClip = () => {
+    const nextClip = currClip == "A" ? "B" : "A";
+    const prevClip = currClip;
+
+    setCurrClip(nextClip);
+
+    if (prevClip === "A") {
+      setClipA(clips.next().value);
+    } else {
+      setClipB(clips.next().value);
+    }
+  };
+
   const handleDecisionClick = (decision: Decision) => {
+    const clip = getCurrentClip();
+
     // Report this decision to the server
     const clipDecision: ClipDecision = {
       decision,
-      clipId: currentClip.clipId,
+      clipId: clip.clipId,
     };
     makeDecision(clipDecision);
 
     // Print out the current summaries
     console.log(JSON.stringify(getDecisions(clipDecision.clipId)));
 
-    // Load in the next clip
-    const clip = clips.next().value;
-    setCurrentClip(clip);
+    // Switch to the other clip and preload the next one
+    toggleCurrClip();
   };
 
+  /**
+   * Here is where we duplicate the components so we can preload the next video and also prevent resizing/flashing on the screen when going to the next one
+   */
   return (
-    <div>
-      <div className="videoContainer">
-        <video
-          key={currentClip.clipId}
-          controls
-          loop
-          autoPlay
-          muted
-          webkit-playsinline
-          playsInline
-        >
-          <source src={currentClip.clipUrl} type="video/mp4" />
-        </video>
-      </div>
-      <div className="decisionContainer">
-        <button onClick={() => handleDecisionClick("left")}>Left</button>
-        <button onClick={() => handleDecisionClick("none")}>None</button>
-        <button onClick={() => handleDecisionClick("right")}>Right</button>
-      </div>
-    </div>
+    <>
+      <DecideTouchImpl
+        hidden={currClip !== "A"}
+        clip={clipA}
+        handleDecisionClick={handleDecisionClick}
+      />
+      <DecideTouchImpl
+        hidden={currClip !== "B"}
+        clip={clipB}
+        handleDecisionClick={handleDecisionClick}
+      />
+    </>
   );
 };
+
+type DecideTouchImplProps = {
+  hidden: boolean;
+  clip: TouchClip;
+  handleDecisionClick: (decision: Decision) => void;
+};
+
+const DecideTouchImpl = ({
+  hidden,
+  clip,
+  handleDecisionClick,
+}: DecideTouchImplProps) => (
+  <div hidden={hidden}>
+    <div className="videoContainer">
+      <video
+        key={clip.clipId}
+        controls
+        loop
+        autoPlay
+        muted
+        webkit-playsinline="true"
+        playsInline
+      >
+        <source src={clip.clipUrl} type="video/mp4" />
+      </video>
+    </div>
+    <div className="decisionContainer">
+      <button onClick={() => handleDecisionClick("left")}>Left</button>
+      <button onClick={() => handleDecisionClick("none")}>None</button>
+      <button onClick={() => handleDecisionClick("right")}>Right</button>
+    </div>
+  </div>
+);
 
 export default DecideTouch;
